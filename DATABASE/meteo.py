0861ccf4b7dd9ec4e7354d1b317351c75592e65e
TAULEIGNE SAMUEL <40196684+SamuelTauleigne@users.noreply.github.com>
2018-07-09 14:31:11 +0200

    Update meteo.py
    Title and description

--- DATABASE/meteo.py
+++ DATABASE/meteo.py
@@ -1,4 +1,5 @@
-#station = 07481
+# meteo.py : Produit un fichier Meteo.csv contenant les relevés météo nécessaires
+
 
 sources = ['synop.201709.csv','synop.201710.csv', 'synop.201711.csv', 'synop.201712.csv', 'synop.201801.csv', 'synop.201802.csv', 'synop.201803.csv', 'synop.201804.csv']
 
@@ -15,20 +16,8 @@ for s in sources:
 
 
 with open('Meteo.csv', 'w') as f:
-    for raw in text: # text[1:] ???
+    for raw in text:
         raw = raw[0].split(";")
-    
-        #num_sta, date, pmer, tend, cod_tend, dd, ff, t, td, u, uv, ww, w1, w2, n, nbas, hbas, cl, cm, ch, pres, niv_bar, geop, tend24, tn12, tn24, tx12, tx24, tminsol, sw, tw, raf10, rafper, per, etat_sol, ht_neige, ssfrai, perssfrai, rr1, rr3, rr6, rr12, rr24, phenspe1, phenspe2, phenspe3, phenspe4, nnuage1, ctype1, hnuage1, nnuage2, ctype2, hnuage2, nnuage3, ctype3, hnuage3, nnuage4, ctype4, hnuage4 = raw.split(";")
-
-        '''
-        if int(num_sta) == 7481:
-        print(date[:8] + ' ' + date[8:] + ' ' + pmer + ' ' + tend + ' ' + cod_tend + ' ' + dd + ' ' + ff + ' ' + t + ' ' + td + ' ' + u + ' ' + uv + ' ' + ww + ' ' + w1 + ' ' + w2 + ' ' + n + ' ' + nbas + ' ' + hbas + ' ' + cl + ' ' + cm + ' ' + ch + ' ' + pres + ' ' + niv_bar + ' ' + geop + ' ' + tend24 + ' ' + tn12 + ' ' + tn24 + ' ' + tx12 + ' ' + tx24 + ' ' + tminsol + ' ' + sw + ' ' + tw + ' ' + raf10 + ' ' + rafper + ' ' + per + ' ' + etat_sol + ' ' + ht_neige + ' ' + ssfrai + ' ' + perssfrai + ' ' + rr1 + ' ' + rr3 + ' ' + rr6 + ' ' + rr12 + ' ' + rr24 + ' ' + phenspe1 + ' ' + phenspe2 + ' ' + phenspe3 + ' ' + phenspe4 + ' ' + nnuage1 + ' ' + ctype1 + ' ' + hnuage1 + ' ' + nnuage2 + ' ' + ctype2 + ' ' + hnuage2 + ' ' + nnuage3 + ' ' + ctype3 + ' ' + hnuage3 + ' ' + nnuage4 + ' ' + ctype4 + ' ' + hnuage4)
-        s += 1
-    
-        if int(num_sta) == 7481:
-        print(date[:8] + ' ' + date[8:] + ' ' + rr1 + ' ' + rr3 + ' ' + rr6 + ' ' + rr12 + ' ' + rr24)
-        s += 1
-        '''
 
         num_sta = raw[0]
         date1 = raw[1][:8]
@@ -42,5 +31,4 @@ with open('Meteo.csv', 'w') as f:
         rr12 = raw[41]
         rr24 = raw[42]
         if int(num_sta) == 7481:
-            #print(date1 + ' ' + date2 + ' ' + ff + ' ' + t + ' ' + u + ' ' + rr1 + ' ' + rr3 + ' ' + rr6 + ' ' + rr12 + ' ' + rr24)
             f.write(date1[0:4]+'-'+date1[4:6]+'-'+date1[6:8] + ',' + date2[0:2]+':'+date2[2:4]+':00' + ',' + ff + ',' + t + ',' + u + ',' + rr1 + ',' + rr3 + ',' + rr6 + ',' + rr12 + ',' + rr24 + '\n')
