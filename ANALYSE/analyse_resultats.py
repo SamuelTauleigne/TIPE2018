0bafa4710dd204cdf2ea216fef7a09f066939fed
TAULEIGNE SAMUEL <40196684+SamuelTauleigne@users.noreply.github.com>
2018-07-09 15:02:42 +0200

    Update analyse_resultats.py
    Title and comments

--- ANALYSE/analyse_resultats.py
+++ ANALYSE/analyse_resultats.py
@@ -1,4 +1,6 @@
-# Analyse des résultats:
+# analyse_resultats.py : Produit les fichiers comportant les résultats de l'analyse
+
+
 
 import Analyse_exe as ae
 from math import sqrt
@@ -6,23 +8,23 @@ import matplotlib.pyplot as plt
 
 
 
-jan=['2018-01-0'+str(k) for k in range(1,10)]+['2018-01-'+str(k) for k in range(10,32)]
-jan_l=['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']*4+['lundi','mardi','mercredi']
+jan = ['2018-01-0'+str(k) for k in range(1,10)] + ['2018-01-'+str(k) for k in range(10,32)]
+jan_l = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']*4+['lundi','mardi','mercredi']
 
-dec= ['2017-12-0'+str(k) for k in range(1,10)]+['2017-12-'+str(k) for k in range(10,32)]
-dec_l= ['vendredi','samedi','dimanche','lundi','mardi','mercredi','jeudi']*4+['vendredi','samedi','dimanche']
+dec = ['2017-12-0'+str(k) for k in range(1,10)] + ['2017-12-'+str(k) for k in range(10,32)]
+dec_l = ['vendredi','samedi','dimanche','lundi','mardi','mercredi','jeudi']*4+['vendredi','samedi','dimanche']
 
-nov= ['2017-11-0'+str(k) for k in range(1,10)]+['2017-11-'+str(k) for k in range(10,31)]
-nov_l=['mercredi','jeudi','vendredi','samedi','dimanche','lundi','mardi']*4+['mercredi','jeudi']
+nov = ['2017-11-0'+str(k) for k in range(1,10)] + ['2017-11-'+str(k) for k in range(10,31)]
+nov_l = ['mercredi','jeudi','vendredi','samedi','dimanche','lundi','mardi']*4+['mercredi','jeudi']
 
-octo= ['2017-10-0'+str(k) for k in range(1,10)]+['2017-10-'+str(k) for k in range(10,32)]
-octo_l=['dimanche','lundi','mardi','mercredi','jeudi','vendredi','samedi']*4+['dimanche','lundi','mardi']
+octo = ['2017-10-0'+str(k) for k in range(1,10)] + ['2017-10-'+str(k) for k in range(10,32)]
+octo_l = ['dimanche','lundi','mardi','mercredi','jeudi','vendredi','samedi']*4+['dimanche','lundi','mardi']
 
-sep=['2017-09-0'+str(k) for k in range(1,10)]+['2017-09-'+str(k) for k in range(10,31)]
-sep_l= ['vendredi','samedi','dimanche','lundi','mardi','mercredi','jeudi']*4+['vendredi','samedi']
+sep = ['2017-09-0'+str(k) for k in range(1,10)] + ['2017-09-'+str(k) for k in range(10,31)]
+sep_l = ['vendredi','samedi','dimanche','lundi','mardi','mercredi','jeudi']*4+['vendredi','samedi']
 
-jours= jan
-jours_l= jan_l
+jours = jan
+jours_l = jan_l
 
 def moy_liste(val_list):
     """Calcule la moyenne des entiers donnés en entrée sous forme de liste."""
@@ -113,10 +115,9 @@ def eff_rmse(r,dv, id_stand, jours, jours_l):
         naif2.append(rmse(obs,prev2))
         prevm2=ae.trace2(id_stand, r,jl,j)
         meth2.append(rmse(obs,prevm2))
-        #print('ii')
         prev3=ae.trace3(r,jl,j, id_stand)
         meth3.append(rmse(obs,prev3))
-        #print('iii')
+
     '''x=[k for k in range(1,len(jours)+1)]
     plt.clf()
     plt.scatter(x,naif1,c='red',label='Naif r dernières valeurs')
@@ -127,11 +128,6 @@ def eff_rmse(r,dv, id_stand, jours, jours_l):
     plt.legend()
     plt.savefig('comparason_eff.png')'''
     return ('1', moy_liste(naif1), ecart_type(naif1), mediane(naif1)), ('2', moy_liste(naif2), ecart_type(naif2), mediane(naif2)), ('3', moy_liste(meth2), ecart_type(meth2), mediane(meth2)), ('4', moy_liste(meth3), ecart_type(meth3), mediane(meth3))
-        
-#('naif1',moy_liste(naif1), ecart_type(naif1)),
-#('naif2',moy_liste(naif2), ecart_type(naif2)),
-#('saison',moy_liste(meth2), ecart_type(meth2)),
-#('similarite',moy_liste(meth3),ecart_type(meth3))
 
 
 def all_res():
