cac46ba84609397daed50e7d559d6ac9070549fe
TAULEIGNE SAMUEL <40196684+SamuelTauleigne@users.noreply.github.com>
2018-07-09 14:56:37 +0200

    Update Analyse_res.py
    Title

--- ANALYSE/Analyse_res.py
+++ ANALYSE/Analyse_res.py
@@ -1,4 +1,6 @@
-# Ce script analyse les résultats obtenus par chacune des méthodes sur des journées données.
+# Analyse_res.py : Analyse les résultats obtenus par chacune des méthodes sur des journées données
+
+
 
 import Analyse_exe as exe # On importe les fonctions permettant d'avoir les résultats de tracé pour chaque méthode.
 
@@ -8,7 +10,6 @@ dates = [('samedi', '2018-01-27'), ('dimanche', '2018-01-28'), ('lundi', '2018-0
 dv = 25
 r = 5 # horizon de prévision
 
-### Ce passage est à adapter aux situations pour lesquelles la liste 'dates' est modifiée.
 [e1, e2, e3, e4] = [0, 0, 0, 0]
 [f1, f2, f3, f4] = [0, 0, 0, 0]
 
@@ -16,26 +17,23 @@ r = 5 # horizon de prévision
 for d in dates:
     # Collecte des données
     orig, naif1, naif2 = exe.trace(r, dv, d[1])
-    #print('naif', len(orig), len(naif1), len(naif2))
     method2 = exe.trace2(r, d[0], d[1])
-    #print('method_2', len(method2))
     method3 = exe.trace3(r, d[0], d[1])
-    #print('method_3', len(method3))
-
-    # Pourquoi (15, r) ?
+    
     for k in range(15, r):
-        # A généraliser
+        
         tem = orig[k]
+        
         n1 = (tem - naif1[k])**2
         n2 = (tem - naif2[k])**2
         m2 = (tem - method2[k])**2
         m3 = (tem - method3[k])**2
-        #print(n1, n2, m2, m3)
+        
         n11 = abs(tem - naif1[k])
         n22 = abs(tem - naif2[k])
         m22 = abs(tem - method2[k])
         m33 = abs(tem - method3[k])
-        #print(n11, n22, m22, m33)
+        
         e1 += n1
         e2 += n2
         e3 += m2
@@ -46,7 +44,6 @@ for d in dates:
         f3 += m22/tem
         f4 += m33/tem
 
-# IDEM
 print(e1, e2, e3, e4)
 print(f1, f2, f3, f4)
 rmse = [(e1/r)**0.5, (e2/r)**0.5, (e3/r)**0.5, (e4/r)**0.5]
